94ae1a7f7126f802f06fe4eb98dbd58b7ec448b9
Irakarama-Laurent123 <96382900+Irakarama-Laurent123@users.noreply.github.com>
2022-09-09 11:59:09 +0200

    Used tokens for request to protected resources

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,46 @@
-import classes from './ProfileForm.module.css';
+import { useContext, useRef } from "react";
+import classes from "./ProfileForm.module.css";
+import AuthContext from "../../store/auth-context";
 
 const ProfileForm = () => {
+  const newPasswordInputRef = useRef();
+  const authContext = useContext(AuthContext);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredNewPassword = newPasswordInputRef.current.value;
+
+    fetch("https://identitytoolkit.github.io/", {
+      method: "POST",
+      body: JSON.stringify({
+        idToken: authContext.token,
+        password: enteredNewPassword,
+        returnSecureToken: false,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      //
+    });
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input
+          ref={newPasswordInputRef}
+          type="password"
+          minLength="7"
+          id="new-password"
+        />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
   );
-}
+};
 
 export default ProfileForm;
